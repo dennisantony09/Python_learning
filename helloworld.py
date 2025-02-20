3d915adad0cb54b112ce04a657edcf8edf6c7c49
dennisantony09 <dennisantony09@gmail.com>
2025-02-20 20:43:41 +0530

    Upload files

---
+++ helloworld.py
@@ -0,0 +1,51 @@
+print("Hello,World!")
+
+a = 2
+b = 5
+print (a+b)
+
+c = 9
+d = 6
+print (c-d)
+
+e = 14
+f= 20
+print(e*f)
+
+g = 380
+h = 5
+print("divide",g/h)
+
+i= 12
+j= 8
+print("modular",i%j)
+
+m = 48
+n = 5
+print(m//n)
+
+x = 2
+y = 4
+print("exponntx",x**y)
+
+x=5
+print(complex(x))
+print(float(x))
+print(bool(x))
+print(str(x))
+
+c="hii"
+#print(float(c))
+print(bool(c))
+d=bool(c)
+e=c
+print(type(d))
+print("--------------------")
+print(e == c)
+
+
+# calculations
+# 1. area
+
+
+# 2. SI
